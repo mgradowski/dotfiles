039e703aff03702d4f244d7d52ccfa32133fc33e
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-08 16:40:54 +0200

    add virt-column

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -1,11 +1,13 @@
 function DeezColorz(color)
-	color = color or "dracula-soft"
-	vim.cmd.colorscheme(color)
+    color = color or "dracula-soft"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#3A444F" })
 end
 
 DeezColorz()
--- neovim/.config/nvim/lua/mgradowski/packer.lua
+++ neovim/.config/nvim/lua/mgradowski/packer.lua
@@ -1,51 +1,53 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use 'Mofiqul/dracula.nvim'
+    use 'Mofiqul/dracula.nvim'
 
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-	use 'nvim-treesitter/playground'
+    use 'nvim-treesitter/playground'
 
-	use 'ThePrimeagen/harpoon'
+    use 'ThePrimeagen/harpoon'
 
-	use 'mbbill/undotree'
+    use 'mbbill/undotree'
 
-	use 'numToStr/Navigator.nvim'
+    use 'numToStr/Navigator.nvim'
 
-	use 'tpope/vim-fugitive'
+    use 'tpope/vim-fugitive'
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{       -- Optional
-				'williamboman/mason.nvim',
-				run = function()
-					pcall(vim.cmd, 'MasonUpdate')
-				end,
-			},
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' }, -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'L3MON4D3/LuaSnip' }, -- Required
-		}
-	}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    }
 
-	use 'jose-elias-alvarez/null-ls.nvim'
+    use 'jose-elias-alvarez/null-ls.nvim'
 
-	use 'jay-babu/mason-null-ls.nvim'
+    use 'jay-babu/mason-null-ls.nvim'
 
-	use 'nvim-treesitter/nvim-treesitter-context'
+    use 'nvim-treesitter/nvim-treesitter-context'
+
+    use "lukas-reineke/virt-column.nvim"
 end)
