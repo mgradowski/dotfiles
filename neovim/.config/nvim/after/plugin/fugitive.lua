0eb06ff1b3a03ab24453720497ac6ebbeda7e474
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-08 14:11:01 +0200

    more gits

--- neovim/.config/nvim/after/plugin/fugitive.lua
+++ neovim/.config/nvim/after/plugin/fugitive.lua
@@ -1,9 +1,13 @@
 -- Toggle fugitive
 vim.keymap.set("n", "<leader>gs", function()
-	local bufname = vim.fn.bufname('fugitive:///*/.git//$')
-	if vim.fn.buflisted(bufname) ~= 0 then
-		vim.cmd [[execute ":bdelete" bufname('fugitive:///*/.git//$')]]
-	else
-		vim.cmd.Git()
-	end
+    local bufname = vim.fn.bufname('fugitive:///*/.git//$')
+    if vim.fn.buflisted(bufname) ~= 0 then
+        vim.cmd [[execute ":bdelete" bufname('fugitive:///*/.git//$')]]
+    else
+        vim.cmd.Git()
+    end
+end)
+
+vim.keymap.set("n", "<leader>gf", function()
+    vim.cmd [[ Git fetch ]]
 end)
