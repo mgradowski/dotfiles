9968b80f54b4d80abcfa3d5bca7533d7af336698
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-08 17:54:39 +0200

    highlight line number

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -2,13 +2,14 @@ function DeezColorz(color)
     color = color or "dracula-soft"
     vim.cmd.colorscheme(color)
 
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-    vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#3A444F" })
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
     vim.api.nvim_set_hl(0, "TreesitterContextSeparator", { fg = "#3A444F" })
+    vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#3A444F" })
 end
 
 DeezColorz()
--- neovim/.config/nvim/lua/mgradowski/set.lua
+++ neovim/.config/nvim/lua/mgradowski/set.lua
@@ -22,4 +22,6 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.opt.cursorline = true
+
 vim.g.mapleader = " "
