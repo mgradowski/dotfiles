f0879b6fd061a79cacac0fbf098cfbbc8d35851d
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-08 14:00:50 +0200

    add git picker

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,22 +1,23 @@
 local telescope = require("telescope")
 telescope.setup {
-	pickers = {
-		find_files = {
-			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-			-- find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-			additional_args = function(_)
-				return { "--hidden", "--glob", "!**/.git/*" }
-			end
-		},
-		live_grep = {
-			additional_args = function(_)
-				return { "--hidden", "--glob", "!**/.git/*" }
-			end
-		}
-	}
+    pickers = {
+        find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            -- find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+            additional_args = function(_)
+                return { "--hidden", "--glob", "!**/.git/*" }
+            end
+        },
+        live_grep = {
+            additional_args = function(_)
+                return { "--hidden", "--glob", "!**/.git/*" }
+            end
+        }
+    }
 }
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
