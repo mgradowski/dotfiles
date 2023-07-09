f56bc64f23e9413d9a6974751cdc7dbada795326
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-09 18:42:52 +0200

    add netrw mappings

--- neovim/.config/nvim/after/plugin/navigator.lua
+++ neovim/.config/nvim/after/plugin/navigator.lua
@@ -6,3 +6,25 @@ vim.keymap.set({ 'n', 't' }, '<C-h>', navigator.left)
 vim.keymap.set({ 'n', 't' }, '<C-l>', navigator.right)
 vim.keymap.set({ 'n', 't' }, '<C-k>', navigator.up)
 vim.keymap.set({ 'n', 't' }, '<C-j>', navigator.down)
+
+-- https://www.reddit.com/r/neovim/comments/ud2w4k/how_to_remap_netrw_to_n_in_keybindingsinitlua/
+vim.api.nvim_create_autocmd('filetype', {
+    pattern = 'netrw',
+    desc = 'Better mappings for netrw',
+    callback = function()
+        local bind = function(lhs, rhs)
+            vim.keymap.set('n', lhs, rhs, { remap = true, buffer = true })
+        end
+
+        bind('<C-h>', navigator.left)
+        bind('<C-l>', navigator.right)
+        bind('<C-k>', navigator.up)
+        bind('<C-j>', navigator.down)
+
+        -- edit new file
+        bind('n', '%')
+
+        -- rename file
+        bind('r', 'R')
+    end
+})
