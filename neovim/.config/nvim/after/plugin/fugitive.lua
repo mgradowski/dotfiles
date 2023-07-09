9743b1aa25175b277f04637f3b6aefe75a65ca9f
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-10 00:11:55 +0200

    fix fugitive toggle

--- neovim/.config/nvim/after/plugin/fugitive.lua
+++ neovim/.config/nvim/after/plugin/fugitive.lua
@@ -1,7 +1,17 @@
 -- Toggle fugitive
 vim.keymap.set("n", "<leader>gs", function()
-    local bufname = vim.fn.bufname('fugitive:///*')
-    if vim.fn.buflisted(bufname) ~= 0 then
+    local fugitive_bufnr = nil
+
+    -- Find the buffer number of the fugitive buffer
+    for bufnr = 1, vim.fn.bufnr("$") do
+        if vim.fn.bufname(bufnr):find('fugitive://') then
+            fugitive_bufnr = bufnr
+            break
+        end
+    end
+
+    -- If the fugitive buffer is listed, delete it. Otherwise, open it.
+    if fugitive_bufnr and vim.fn.buflisted(fugitive_bufnr) ~= 0 then
         vim.cmd [[execute ":bdelete" bufname('fugitive:///*')]]
     else
         if vim.fn.FugitiveHead() ~= '' then
