8a6386196c9d9e6bf9bbd29561ce7acf374cf43a
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-11 20:39:15 +0200

    polish lualine

--- neovim/.config/nvim/after/plugin/lualine.lua
+++ neovim/.config/nvim/after/plugin/lualine.lua
@@ -1,12 +1,27 @@
 local lualine = require('lualine')
 local theme1 = require('lualine.themes.base16')
 
+theme1.command.a.fg = theme1.command.a.bg
+theme1.command.a.bg = "none"
 theme1.command.b.bg = "none"
 theme1.command.c.bg = "none"
+
+theme1.insert.a.fg = theme1.insert.a.bg
+theme1.insert.a.bg = "none"
 theme1.insert.b.bg = "none"
+
+theme1.normal.a.bold = "bold"
+theme1.normal.a.fg = theme1.normal.a.bg
+theme1.normal.a.bg = "none"
 theme1.normal.b.bg = "none"
 theme1.normal.c.bg = "none"
+
+theme1.replace.a.fg = theme1.replace.a.bg
+theme1.replace.a.bg = "none"
 theme1.replace.b.bg = "none"
+
+theme1.visual.a.fg = theme1.visual.a.bg
+theme1.visual.a.bg = "none"
 theme1.visual.b.bg = "none"
 
 lualine.setup({
@@ -37,11 +52,13 @@ lualine.setup({
                 'diagnostics',
                 always_visible = true,
                 symbols = { error = '', warn = '', info = '', hint = '' },
+                update_in_insert = true,
             },
         },
         lualine_y = {
             'encoding',
-            'filetype'
+            'filetype',
+            'location',
         },
         lualine_z = {},
     }
