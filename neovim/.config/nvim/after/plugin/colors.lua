69c7a2157b96b00c70d7f2d4d8c4afc99d6dbfba
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2023-07-15 21:00:26 +0200

    highlight cursorline & add alacritty transparency

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -2,7 +2,6 @@ function DeezColorz(color)
     color = color or "dracula-soft"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
@@ -12,6 +11,7 @@ function DeezColorz(color)
     vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#3A444F" })
     vim.api.nvim_set_hl(0, "IndentBlanklineChar", { fg = "#3A444F" })
     vim.api.nvim_set_hl(0, "DiffAdd", { fg = "#87e58e" })
+    vim.api.nvim_set_hl(0, "Cursorline", { bg = "#313131" })
 end
 
 DeezColorz()
--- neovim/.config/nvim/after/plugin/git-blame.lua
+++ neovim/.config/nvim/after/plugin/git-blame.lua
@@ -1,2 +1,4 @@
 vim.g.gitblame_virtual_text_column = 80
 vim.g.gitblame_date_format = "%r"
+vim.g.gitblame_message_template = ' <author> • <date> • <summary>' .. string.rep(" ", 100)
+vim.g.gitblame_message_when_not_committed = ' Uncommited' .. string.rep(" ", 100)
