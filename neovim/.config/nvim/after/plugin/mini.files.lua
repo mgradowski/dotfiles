83179d3275b1225ea31fcafafbc7875ea7130eea
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2024-02-03 23:05:26 +0100

    enable mini.files preview

--- neovim/.config/nvim/after/plugin/mini.files.lua
+++ neovim/.config/nvim/after/plugin/mini.files.lua
@@ -3,6 +3,10 @@ local minifiles = require("mini.files")
 minifiles.setup({
     content = {
         prefix = function() end,
+    },
+    windows = {
+        preview = true,
+        width_preview = 80,
     }
 })
 
