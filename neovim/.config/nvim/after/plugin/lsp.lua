cd3e32ce2b15033d1a4348673a790e114e0fd073
Mikołaj Gradowski <mikolaj.gradowski@hotmail.com>
2024-02-04 18:01:23 +0100

    use telescope instead of quickfix for LSP

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -7,6 +7,7 @@ local mason_lspconfig = require("mason-lspconfig")
 local mason_null_ls = require("mason-null-ls")
 local neodev = require("neodev")
 local null_ls = require("null-ls")
+local telescope_builtin = require("telescope.builtin")
 
 
 vim.api.nvim_create_autocmd(
@@ -18,7 +19,8 @@ vim.api.nvim_create_autocmd(
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+            vim.keymap.set("n", "<leader>vrr", telescope_builtin.lsp_references, opts)
+            vim.keymap.set("n", "<leader>vws", telescope_builtin.lsp_workspace_symbols, opts)
             vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         end
     }
