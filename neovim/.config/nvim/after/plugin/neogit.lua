5f610c50ecf779b1aea3e3a51ec634dbec613ec1
Mikołaj Gradowski <hello@mgradow.ski>
2025-10-16 15:57:23 +0200

    fix neogit keymap

--- neovim/.config/nvim/after/plugin/neogit.lua
+++ neovim/.config/nvim/after/plugin/neogit.lua
@@ -3,23 +3,7 @@ local neogit = require("neogit")
 
 neogit.setup({})
 
--- Toggle Neogit
 vim.keymap.set("n", "<leader>gs", function()
-    local neogit_bufnr = nil
-
-    -- Find the buffer number of the Neogit buffer
-    ---@diagnostic disable-next-line: param-type-mismatch
-    for bufnr = 1, vim.fn.bufnr("$") do
-        if vim.fn.bufname(bufnr):find("NeogitStatus") then
-            neogit_bufnr = bufnr
-            break
-        end
-    end
-
-    if neogit_bufnr then
-        vim.cmd [[ execute ":bdelete" bufname('fugitive:///*') ]]
-    else
-        minifiles.close()
-        vim.cmd [[ Neogit ]]
-    end
+    minifiles.close()
+    vim.cmd [[ Neogit ]]
 end)
