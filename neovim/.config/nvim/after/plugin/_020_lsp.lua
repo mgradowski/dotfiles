3064463c6ddc14005f7f51240be339bbc2246bfe
Mikołaj Gradowski <hello@mgradow.ski>
2025-10-17 20:00:55 +0200

    LSP binding descriptions and AutoformatToggle

--- neovim/.config/nvim/after/plugin/_020_lsp.lua
+++ neovim/.config/nvim/after/plugin/_020_lsp.lua
@@ -1,27 +1,26 @@
 local lazydev = require('lazydev')
-local mason_lspconfig = require("mason-lspconfig")
+local mason_lspconfig =
+    require("mason-lspconfig")
 local mason_null_ls = require("mason-null-ls")
-local null_ls = require('null-ls')
+local
+null_ls = require('null-ls')
 
 mason_lspconfig.setup({
     handlers = {
         -- default handler
         function(server)
             vim.lsp.config(server, {
-                capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                capabilities =
+                    require('cmp_nvim_lsp').default_capabilities(),
             })
-        end,
-    },
+        end, },
 })
 
 lazydev.setup()
 
 mason_null_ls.setup({
     ensure_installed = {
-        "autoflake",
-        "isort",
         "ruff",
-        "black",
     },
     handlers = {},
     automatic_installation = false,
@@ -29,40 +28,107 @@ mason_null_ls.setup({
 
 null_ls.setup()
 
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(event)
+        vim.keymap.set("n", "gd",
+            function() vim.lsp.buf.definition() end,
+            {
+                desc = "LSP: Go to definition",
+                buffer = event.buf,
+                remap =
+                    true,
+            })
 
-vim.api.nvim_create_autocmd(
-    "LspAttach",
-    {
-        callback = function(event)
-            local opts = { buffer = event.bufnr, remap = true }
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-            vim.keymap.set("n", "K", function()
+        vim.keymap.set("n", "gi",
+            function() vim.lsp.buf.implementation() end,
+            {
+                desc = "LSP: Go to implementation",
+                buffer = event.buf,
+                remap =
+                    true
+            })
+
+        vim.keymap.set(
+            "n", "K",
+            function()
                 if require('dap').session() then
                     require('dap.ui.widgets').hover()
                 else
                     vim.lsp.buf.hover()
                 end
-            end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", require("telescope.builtin").lsp_references, opts)
-            vim.keymap.set("n", "<leader>vws", require("telescope.builtin").lsp_dynamic_workspace_symbols, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        end
-    }
+            end,
+            {
+                desc = "LSP/DAP: Hover",
+                buffer = event.buf,
+                remap = true
+            })
+
+        vim.keymap.set(
+            "n", "<leader>vca",
+            function() vim.lsp.buf.code_action() end,
+            {
+                desc = "LSP: Show code actions",
+                buffer = event.buf,
+                remap = true
+            })
+
+        vim.keymap.set(
+            "n", "<leader>vcr",
+            require("telescope.builtin").lsp_references,
+            {
+                desc = "LSP: Show references",
+                buffer = event.buf,
+                remap = true
+            })
+
+        vim.keymap.set("n", "<leader>vcs", require("telescope.builtin").lsp_dynamic_workspace_symbols,
+            {
+                desc = "LSP: Show workspace symbols",
+                buffer = event.buf,
+                remap = true
+            })
+
+        vim.keymap.set("n", "<leader>vcn", function() vim.lsp.buf.rename() end,
+            {
+                desc = "LSP: Rename",
+                buffer = event.buf,
+                remap = true
+            })
+    end
+}
 )
 
+vim.g.autoformat_enabled = true
+
+local autoformat_group = vim.api.nvim_create_augroup("AutoFormat", { clear = true })
 vim.api.nvim_create_autocmd(
     "BufWritePre",
     {
+        group = autoformat_group,
         callback = function(event)
-            vim.lsp.buf.format({
-                async = false,
-                bufnr = event.bufnr,
-            })
+            if vim.g.autoformat_enabled then
+                vim.lsp.buf.format({
+                    async = false,
+                    bufnr = event.buf,
+                })
+            end
         end
     }
 )
+vim.api.nvim_create_user_command("AutoformatEnable", function()
+    vim.g.autoformat_enabled = true
+    vim.notify("Autoformatting enabled")
+end, {})
+
+vim.api.nvim_create_user_command("AutoformatDisable", function()
+    vim.g.autoformat_enabled = false
+    vim.notify("Autoformatting disabled")
+end, {})
+
+vim.api.nvim_create_user_command("AutoformatToggle", function()
+    vim.g.autoformat_enabled = not vim.g.autoformat_enabled
+    vim.notify("Autoformatting " .. (vim.g.autoformat_enabled and "enabled" or "disabled"))
+end, {})
 
 vim.lsp.config('beancount', {
     init_options = {
