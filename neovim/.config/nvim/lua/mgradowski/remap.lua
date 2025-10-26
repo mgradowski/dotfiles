d71eb963985f89fc1a2d89de810a2ede3dd6dafb
Mikołaj Gradowski <hello@mgradow.ski>
2025-10-26 02:34:06 +0200

    neovim: add clipboard bindings

--- neovim/.config/nvim/lua/mgradowski/remap.lua
+++ neovim/.config/nvim/lua/mgradowski/remap.lua
@@ -54,3 +54,19 @@ vim.keymap.set("i", "<C-f>", "<Right>")
 vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+
+if vim.g.neovide then
+    vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
+    vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
+    vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
+    vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
+    vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
+    vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+
+vim.keymap.set('c', '<C-f>', '<Nop>') -- Paste command mode
