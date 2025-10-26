8f0da4d9a8577df498cc0dff504da1eacff4320b
Mikołaj Gradowski <hello@mgradow.ski>
2025-10-27 00:54:11 +0100

    neovide: fix comment keymap

--- neovim/.config/nvim/after/plugin/comment.lua
+++ neovim/.config/nvim/after/plugin/comment.lua
@@ -2,5 +2,11 @@ local comment = require('Comment')
 comment.setup()
 
 
-vim.keymap.set("n", "<C-_>", "gcc", { remap = true })
-vim.keymap.set("v", "<C-_>", "gcgv", { remap = true })
+if vim.g.neovide then
+    vim.keymap.set("n", "<C-/>", "gcc", { remap = true })
+    vim.keymap.set("v", "<C-/>", "gcgv", { remap = true })
+else
+    --  alacritty workaround
+    vim.keymap.set("n", "<C-_>", "gcc", { remap = true })
+    vim.keymap.set("v", "<C-_>", "gcgv", { remap = true })
+end
