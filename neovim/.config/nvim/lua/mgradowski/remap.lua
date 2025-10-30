dd51b60b96a1480a424c102af886e10326111c64
Mikołaj Gradowski <hello@mgradow.ski>
2025-10-30 02:52:27 +0100

    fix cmd+v in terminal mode

--- neovim/.config/nvim/lua/mgradowski/remap.lua
+++ neovim/.config/nvim/lua/mgradowski/remap.lua
@@ -50,14 +50,6 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_prev)
 
 
 if vim.g.neovide then
-    vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
-    vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
-    vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
-    vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
-    vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
-    vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
-
-
     -- stupid place to keep this but whatever
     local _initial_initial_scale_factor = 0.9
     vim.g.neovide_scale_factor = _initial_initial_scale_factor
@@ -76,9 +68,14 @@ if vim.g.neovide then
         end)
 end
 
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<D-s>', ':w<CR>')          -- Save
+vim.keymap.set('v', '<D-c>', '"+y')             -- Copy
+vim.keymap.set('n', '<D-v>', '"+P')             -- Paste normal mode
+vim.keymap.set('v', '<D-v>', '"+P')             -- Paste visual mode
+vim.keymap.set('c', '<D-v>', '<C-R>+')          -- Paste command mode
+vim.keymap.set('t', '<D-v>', '<C-\\><C-N>"+Pi') -- Paste terminal mode
+vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli')     -- Paste insert mode
 vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 
 vim.keymap.set('c', '<C-f>', '<Nop>')
