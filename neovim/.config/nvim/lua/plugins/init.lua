499fd8cb2ab2b193e5281f78cf8633dfbec37d9f
Mikołaj Gradowski <hello@mgradow.ski>
2026-06-06 00:02:08 +0200

    neovim: fix ft=vimwiki when outside ~/vimwiki

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -31,7 +31,17 @@ return {
         branch = 'main',
     },
     'nvim-treesitter/nvim-treesitter-context',
-    'vimwiki/vimwiki',
+    {
+        'vimwiki/vimwiki',
+        init = function()
+            vim.g.vimwiki_list = { {
+                path = '~/vimwiki',
+                syntax = 'markdown',
+                ext = '.md',
+            } }
+            vim.g.vimwiki_global_ext = 0
+        end,
+    },
     'windwp/nvim-autopairs',
     'codethread/qmk.nvim',
     'folke/which-key.nvim',
